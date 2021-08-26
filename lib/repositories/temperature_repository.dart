debd9f3689dd0d188d7da2aaf0fe9df467d0fa33
AlineGimenez <alinegimenezdecastro@hotmail.com>
2021-08-25 23:26:21 -0300

    Alteração para conexão APP -> ESP32

--- lib/repositories/temperature_repository.dart
+++ lib/repositories/temperature_repository.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class TemperatureRepository {
-  Future<String> solicitarTemperatura() async {
+  Future<dynamic> solicitarTemperatura() async {
     /*var url = Uri.parse(
         "https://3000-harlequin-wildcat-7ivobsaz.ws-us04.gitpod.io/cadastrarmotorista");*/
-    String url = 'https://proteja-se-api.herokuapp.com/solicitartemperatura';
+    //String url = 'https://proteja-se-api.herokuapp.com/solicitartemperatura';
+    String url = 'http://protejase.hopto.org/testinho';
 
     var response = await http.get(Uri.parse(url));
-    print(response.body);
+    print(response.statusCode);
 
     return response.body;
   }
